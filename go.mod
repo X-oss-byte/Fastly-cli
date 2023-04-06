ce7e098f48bbc9a60341a138206296abb16b1438
Integralist <integralist@fastly.com>
2023-04-06 13:00:04 +0100

    resolve conflicts

--- go.mod
+++ go.mod
@@ -30,6 +30,7 @@ require (
 
 require (
 	github.com/fastly/go-fastly/v7 v7.5.5
+	github.com/hashicorp/cap v0.2.0
 	github.com/kennygrant/sanitize v1.2.4
 	github.com/mholt/archiver v3.1.1+incompatible
 	github.com/otiai10/copy v1.9.0
@@ -40,11 +41,16 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/coreos/go-oidc/v3 v3.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/jsonapi v1.0.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.0.0 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
 	github.com/klauspost/compress v1.16.0 // indirect
 	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -52,11 +58,18 @@ require (
 	github.com/nwaples/rardecode v1.1.2 // indirect
 	github.com/peterhellberg/link v1.1.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.8 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.29.1 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/commands/authenticate/root.go
+++ pkg/commands/authenticate/root.go
@@ -1,11 +1,18 @@
 package authenticate
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
+	"strings"
 
 	"github.com/fastly/cli/pkg/cmd"
+	"github.com/fastly/cli/pkg/debug"
+	fsterr "github.com/fastly/cli/pkg/errors"
 	"github.com/fastly/cli/pkg/global"
+	"github.com/fastly/cli/pkg/text"
+	"github.com/hashicorp/cap/oidc"
 )
 
 // RootCommand is the parent command for all subcommands in this package.
@@ -13,6 +20,21 @@ import (
 type RootCommand struct {
 	cmd.Base
 }
+
+// AuthRemediation is a generic remediation message for an error authorizing.
+const AuthRemediation = "Please re-run the command. If the problem persists, please file an issue: https://github.com/fastly/cli/issues/new?labels=bug&template=bug_report.md"
+
+// Auth0CLIAppURL is the Auth0 device code URL.
+const Auth0CLIAppURL = "https://dev-37kjpso9.us.auth0.com"
+
+// Auth0ClientID is the Auth0 Client ID.
+const Auth0ClientID = "7TAnqT4DTDhJTyXk9aXcuD48JoHRXK2X"
+
+// Auth0Audience is the unique identifier of the API your app wants to access.
+const Auth0Audience = "https://api.secretcdn-stg.net/"
+
+// Auth0RedirectURL is the endpoint Auth0 will pass an authorization code to.
+const Auth0RedirectURL = "http://localhost:8080/callback"
 
 // NewRootCommand returns a new command registered in the parent.
 func NewRootCommand(parent cmd.Registerer, g *global.Data) *RootCommand {
@@ -24,6 +46,189 @@ func NewRootCommand(parent cmd.Registerer, g *global.Data) *RootCommand {
 
 // Exec implements the command interface.
 func (c *RootCommand) Exec(_ io.Reader, out io.Writer) error {
-	fmt.Println("...")
+	verifier, err := oidc.NewCodeVerifier()
+	if err != nil {
+		return fsterr.RemediationError{
+			Inner:       fmt.Errorf("failed to generate a code verifier: %w", err),
+			Remediation: AuthRemediation,
+		}
+	}
+
+	result := make(chan authorizationResult)
+
+	s := server{
+		result:   result,
+		router:   http.NewServeMux(),
+		verifier: verifier,
+	}
+	s.routes()
+
+	var serverErr error
+
+	go func() {
+		err := s.startServer()
+		if err != nil {
+			serverErr = err
+		}
+	}()
+
+	if serverErr != nil {
+		return serverErr
+	}
+
+	text.Info(out, "Starting localhost server to handle the authentication flow.")
+
+	authorizationURL, err := generateAuthorizationURL(verifier)
+	if err != nil {
+		return fsterr.RemediationError{
+			Inner:       fmt.Errorf("failed to generate an authorization URL: %w", err),
+			Remediation: AuthRemediation,
+		}
+	}
+
+	text.Break(out)
+	text.Description(out, "Please open the following URL", authorizationURL)
+
+	ar := <-result
+	if ar.err != nil || ar.jwt.AccessToken == "" {
+		return fsterr.RemediationError{
+			Inner:       fmt.Errorf("failed to authorize: %w", ar.err),
+			Remediation: AuthRemediation,
+		}
+	}
+
+	// TODO: call Fastly API's /internal/poc/authn/callback endpoint.
+	// This is to exchange the access token for an API token.
+
+	// TODO: Persist token to application configuration.
+	// How does this work with `fastly profile` (set on the default)?
+
 	return nil
+}
+
+type server struct {
+	result   chan authorizationResult
+	router   *http.ServeMux
+	verifier *oidc.S256Verifier
+}
+
+func (s *server) startServer() error {
+	// TODO: Consider using a random port to avoid local network conflicts.
+	// Chat with Auth0 about how to use a random port.
+	err := http.ListenAndServe(":8080", s.router)
+	if err != nil {
+		return fsterr.RemediationError{
+			Inner:       fmt.Errorf("failed to start local server: %w", err),
+			Remediation: AuthRemediation,
+		}
+	}
+	return nil
+}
+
+func (s *server) routes() {
+	s.router.HandleFunc("/callback", s.handleCallback())
+}
+
+func (s *server) handleCallback() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		authorizationCode := r.URL.Query().Get("code")
+		if authorizationCode == "" {
+			fmt.Fprint(w, "ERROR: no authorization code returned\n")
+			s.result <- authorizationResult{
+				err: fmt.Errorf("no authorization code returned"),
+			}
+			return
+		}
+
+		// Exchange the authorization code and the code verifier for a JWT.
+		codeVerifier := s.verifier.Verifier()
+		jwt, err := getJWT(codeVerifier, authorizationCode)
+		if err != nil || jwt.AccessToken == "" {
+			fmt.Fprint(w, "ERROR: no access token returned\n")
+			s.result <- authorizationResult{
+				err: fmt.Errorf("no access token returned"),
+			}
+			return
+		}
+
+		fmt.Fprint(w, "Authenticated successfully. Please close this page and return to the Fastly CLI in your terminal.")
+		s.result <- authorizationResult{
+			jwt: jwt,
+		}
+	}
+}
+
+type authorizationResult struct {
+	err error
+	jwt JWT
+}
+
+func getJWT(codeVerifier, authorizationCode string) (JWT, error) {
+	path := "/oauth/token"
+
+	payload := fmt.Sprintf(
+		"grant_type=authorization_code&client_id=%s&code_verifier=%s&code=%s&redirect_uri=%s",
+		Auth0ClientID,
+		codeVerifier,
+		authorizationCode,
+		"http://localhost:8080", // NOTE: not redirected to, just a security check.
+	)
+
+	req, err := http.NewRequest("POST", Auth0CLIAppURL+path, strings.NewReader(payload))
+	if err != nil {
+		return JWT{}, err
+	}
+
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return JWT{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return JWT{}, err
+	}
+
+	var jwt JWT
+	err = json.Unmarshal(body, &jwt)
+	if err != nil {
+		return JWT{}, err
+	}
+
+	// FIXME: Delete this line.
+	_ = debug.PrintStruct(jwt)
+
+	return jwt, nil
+}
+
+// JWT is the API response for an Auth0 Token request.
+type JWT struct {
+	// AccessToken can be exchanged for a Fastly API token.
+	AccessToken string `json:"access_token"`
+	// ExpiresIn indicates the lifetime (in seconds) of the access token.
+	ExpiresIn int `json:"expires_in"`
+	// IDToken contains user information that must be decoded and extracted.
+	IDToken string `json:"id_token"`
+	// TokenType indicates which HTTP authentication scheme is used (e.g. Bearer).
+	TokenType string `json:"token_type"`
+}
+
+func generateAuthorizationURL(verifier *oidc.S256Verifier) (string, error) {
+	challenge, err := oidc.CreateCodeChallenge(verifier)
+	if err != nil {
+		return "", err
+	}
+
+	authorizationURL := fmt.Sprintf(
+		"%s/authorize?audience=%s"+
+			"&scope=openid"+
+			"&response_type=code&client_id=%s"+
+			"&code_challenge=%s"+
+			"&code_challenge_method=S256&redirect_uri=%s",
+		Auth0CLIAppURL, Auth0Audience, Auth0ClientID, challenge, Auth0RedirectURL)
+
+	return authorizationURL, nil
 }
